e7c453bcdcdc9e64f374f21c29894bb95f9a6d79
AAZinoviev <alexander.alexeyevich.zinoviev@yandex.ru>
2019-06-30 14:51:00 +0300

    Add interface required by task

---
+++ IVector3f.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    interface IVector3f
+    {
+        float X { get; }
+        float Y { get; }
+        float Z { get; }
+
+        IVector3f Sum(IVector3f other);
+        IVector3f Diff(IVector3f other);
+
+        float Dot(IVector3f other);
+        float Cos(IVector3f other);
+
+        float Lenght();
+    }
+}
