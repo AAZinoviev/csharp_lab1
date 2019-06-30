fc3233c477a3a238cd90ead2049c198347c4adde
AAZinoviev <alexander.alexeyevich.zinoviev@yandex.ru>
2019-06-30 15:20:41 +0300

    Add Vector realization

---
+++ Vector3f.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    class Vector3f : IVector3f
+    {
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float Z { get; private set; }
+
+        static Vector3f()
+        {
+            Console.WriteLine("static constructor of base class Vector3f is called");
+        }
+
+        public Vector3f()
+        {
+            X = 0f;
+            Y = 0f;
+            Z = 0f;
+        }
+
+        public Vector3f(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public IVector3f Sum(IVector3f other) => new Vector3f(X + other.X, Y + other.Y, Z + other.Z);
+
+        public IVector3f Diff(IVector3f other) => new Vector3f(X - other.X, Y - other.Y, Z - other.Z);
+
+        public float Dot(IVector3f other) => X * other.X + Y * other.Y + Z * other.Z;
+
+        public float Lenght() => (float) Math.Sqrt(X*X + Y*Y + Z*Z);
+
+        public float Cos(IVector3f other) => Dot(other) / (Lenght() * other.Lenght());
+    }
+}
